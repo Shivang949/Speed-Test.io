a57caf9f6e1b31c5348ac4910e4599f38cec80ad
Shivang949 <71367082+Shivang949@users.noreply.github.com>
2021-07-18 14:30:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
         res = s.results.dict()
         download = round(res["download"]/100000000, 2)
         upload = round(res["upload"]/100000000, 2)
-        print("-->Download Speed: {:.2f} kb/s\n-->Upload Speed: {:.2f} Kb/s".format(download, upload))
+        print("-->Download Speed: {:.2f} Mb/s\n-->Upload Speed: {:.2f} Mb/s".format(download, upload))
         return render_template('index.html', download=download, upload=upload)
     else:
         return render_template('index.html', download=0, upload=0)
